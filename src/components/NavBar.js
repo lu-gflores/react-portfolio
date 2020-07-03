95c334e554fa238739554da7a90486a35a929414
George Flores <georgeflores886@gmail.com>
2020-07-03 19:01:48 -0400

    set up navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,62 @@
 import React, { Fragment } from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
-    return (
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <Link className="navbar-item" href="https://bulma.io">
-            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
-          </Link>
-      
-          <Link role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </Link>
-        </div>
-      
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <a className="navbar-item">
-              About Me
-            </a>
-      
-            <a className="navbar-item">
-              Portfolio
-            </a>
-    
-              <a className="navbar-item">
-                Contact Me
-              </a>
-          </div>
-        </div>
-      </nav>
-    
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <Link className="navbar-brand" to="/">
+          George Flores
+        </Link>
+        
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
 
-    )
+
+      </div>
+        <div className="navbar-menu">
+          <ul className="navbar-end">
+            <li className='navbar-item'>
+              <Link to='/'
+                className={window.location.pathname === '/' || window.location.pathname === '/about'
+                  ? 'nav-link active' : 'nav-link'
+                }
+              >
+                About
+          </Link>
+            </li>
+            
+            <li className='navbar-item'>
+              <Link to='/'
+                className={window.location.pathname === '/' || window.location.pathname === '/portfolio'
+                  ? 'nav-link active' : 'nav-link'
+                }
+              >
+                Portfolio
+          </Link>
+
+            </li>
+            <li className='navbar-item'>
+              <Link to='/'
+                className={window.location.pathname === '/' || window.location.pathname === '/contact'
+                  ? 'nav-link active' : 'nav-link'
+                }
+              >
+                Contact
+          </Link>
+            </li>
+          </ul>
+        </div>
+
+
+
+    </nav>
+
+
+  )
 }
 
 export default NavBar;
