4d6b60e60c257b64ead751df6fd96bcc71ec1d8d
George Flores <georgeflores886@gmail.com>
2021-06-18 12:48:04 -0400

    added images and updated info

--- src/components/Me.js
+++ src/components/Me.js
@@ -3,7 +3,9 @@ import Avatar from "../images/avatar.jpg"
 import HTMLlogo from '../images/html5logo.png'
 import JavaScriptLogo from '../images/javascript.png'
 import CSSLogo from '../images/css3.png'
+import BootstrapLogo from '../images/Bootstrap-logo.png'
 import ReactLogo from '../images/reactlogo.png'
+import NodeLogo from '../images/node-js.png'
 import '../styles/Me.css'
 const Me = () => {
     const iconStyle = {
@@ -32,38 +34,44 @@ const Me = () => {
                 <div className="columns is-multiline">
                     <div className="column is-three-quarters">
                         <p className="subtitle is-4 intro has-text-center" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                            I'm a full stack developer and a Computer Science graduate of UNC Charlotte based around Charlotte, NC. While I have a diverse background
-                            in the Computer Science field, I find myself drawn to the development field for its creativity, problem solving, and flexibility. 
-                    </p>
+                            I'm a full stack web developer and a Computer Science graduate of UNC Charlotte based around Charlotte, NC. While I have a diverse background
+                            in the Computer Science field, I find myself drawn to the development field for its creativity, problem solving, and flexibility.
+                        </p>
                     </div>
 
                     <div className="column is-one-quarter">
                         <figure className="image" data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                            <img className="is-rounded" src={Avatar} alt="Avatar of George Flores" style={{ width: "250px", height: "250px", paddingBottom:"1rem" }} />
+                            <img className="is-rounded" src={Avatar} alt="Avatar of George Flores" style={{ width: "250px", height: "250px", paddingBottom: "1rem" }} />
                         </figure>
                     </div>
 
                     <div className="column is-half">
-                    <h2 className="title is-3" data-aos="fade-right" data-aos-offset="200">What I can do</h2>
-                    <figure className="image is-128x128 is-inline-block" data-aos="fade-right" data-aos-offset="200">
-                         <img src={HTMLlogo} alt="html logo" />
-                    </figure>
-                    <figure className="image is-128x128 is-inline-block" data-aos="fade-right" data-aos-offset="200">
-                         <img src={CSSLogo} alt="css logo" />
-                    </figure>
-                    <figure className="image is-128x128 is-inline-block" data-aos="fade-right" data-aos-offset="200">
-                         <img src={JavaScriptLogo} alt="Javascript logo" />
-                    </figure>
-                    <figure className="image is-128x128 is-inline-block" data-aos="fade-right" data-aos-offset="200">
-                         <img src={ReactLogo} alt="React logo" />
-                    </figure>
+                        <h2 className="title is-3" data-aos="fade-right" data-aos-offset="200">What I can do</h2>
+                        <figure className="image is-128x128 is-inline-block" data-aos="fade-right" data-aos-offset="200">
+                            <img src={HTMLlogo} alt="html logo" />
+                        </figure>
+                        <figure className="image is-128x128 is-inline-block" data-aos="fade-right" data-aos-offset="200">
+                            <img src={CSSLogo} alt="css logo" />
+                        </figure>
+                        <figure className="image is-128x128 is-inline-block" data-aos="fade-right" data-aos-offset="200">
+                            <img src={JavaScriptLogo} alt="Javascript logo" />
+                        </figure>
+                        <figure className="image is-128x128 is-inline-block" data-aos="fade-right" data-aos-offset="200">
+                            <img src={BootstrapLogo} alt="Bootstrap logo" />
+                        </figure>
+                        <figure className="image is-128x128 is-inline-block" data-aos="fade-right" data-aos-offset="200">
+                            <img src={ReactLogo} alt="React logo" />
+                        </figure>
+                        <figure className="image is-128x128 is-inline-block" data-aos="fade-right" data-aos-offset="200">
+                            <img src={NodeLogo} alt="Node.js logo" />
+                        </figure>
                     </div>
 
                     <div className="column is-half">
                         <p className="subtitle is-4 intro" data-aos="fade-left" data-aos-offset="200">
-                           While I have experience in these set languages, I also have worked in different concepts in Full Stack, including MongoDB, NodeJS, Express, 
-                           Handlebars, and jQuery.
-                    </p>
+                            While I have experience in the fundamentals of web development, I also have worked in different concepts in Full Stack, including MongoDB, NodeJS, Express,
+                            Handlebars, and jQuery. I'm always eager to learn more technology along the way too!
+                        </p>
                     </div>
                 </div>
 
